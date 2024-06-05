03a2f26a58c2d5ee04d647022da102d092a4d586
irisk <irisk@DESKTOP-A19KBRS>
2024-06-05 19:10:55 +0300

    db

--- Form1.cs
+++ Form1.cs
@@ -7,14 +7,48 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Npgsql;
+
+
 
 namespace MiSPIS
 {
     public partial class Form1 : Form
     {
+
+        string sql = "Server = Localhost; Port=5412;Database=Kurs;";
+       
+
+        //подключение базы данных
         public Form1()
         {
             InitializeComponent();
+
+            sqlConectionReader();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void sqlConectionReader()
+        {
+            NpgsqlConnection sqlConnession = new NpgsqlConnection();// считывает данные sql
+            sqlConnession.Open();// открываем соединение с БД
+            NpgsqlCommand command = new NpgsqlCommand(sql);//инициализация класса
+            command.Connection = sqlConnession;//передаем строку подключения к БД
+            command.CommandType = CommandType.Text;// тип команды
+            command.CommandText = "SELECT*FROM Clients"; //команда
+            NpgsqlDataReader dataReader = command.ExecuteReader();//для извлечения данных из БД
+            if (dataReader.HasRows) //проверяем содержит ли одну или несколько строк
+            {
+                DataTable data = new DataTable();
+                data.Load(dataReader);
+                dataGridView1 = data;//таблица
+            }
+            command.Dispose();
+            sqlConnession.Close();//закрываем соединени с БД
         }
     }
 }
