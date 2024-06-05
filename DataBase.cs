f08f194bd756841f5d2b0921122d542e8fa64a0c
irisk <irisk@DESKTOP-A19KBRS>
2024-06-05 22:49:21 +0300

    bd

---
+++ DataBase.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace MiSPIS
+{
+     class DataBase
+    {
+        MySqlConnection connection = new MySqlConnection("server=localhost;port=3306;username=root;password=123456789;database=sys;Integrated Security=True");
+
+
+        public void openConnection()
+        { 
+            if (connection.State == System.Data.ConnectionState.Closed)
+                connection.Open();
+        }
+
+        public void closeConnection()
+        {
+            if (connection.State == System.Data.ConnectionState.Open)
+                connection.Close();
+        }
+
+        public  MySqlConnection GetConnection()
+        {
+            return connection;
+        }
+
+    }
+}
--- Form1.cs
+++ Form1.cs
@@ -16,15 +16,14 @@ namespace MiSPIS
     public partial class Form1 : Form
     {
 
-        string sql = "Server = Localhost; Port=5412;Database=Kurs;";
-       
+
+
 
         //подключение базы данных
         public Form1()
         {
             InitializeComponent();
 
-            sqlConectionReader();
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -32,23 +31,5 @@ namespace MiSPIS
 
         }
 
-        private void sqlConectionReader()
-        {
-            NpgsqlConnection sqlConnession = new NpgsqlConnection();// считывает данные sql
-            sqlConnession.Open();// открываем соединение с БД
-            NpgsqlCommand command = new NpgsqlCommand(sql);//инициализация класса
-            command.Connection = sqlConnession;//передаем строку подключения к БД
-            command.CommandType = CommandType.Text;// тип команды
-            command.CommandText = "SELECT*FROM Clients"; //команда
-            NpgsqlDataReader dataReader = command.ExecuteReader();//для извлечения данных из БД
-            if (dataReader.HasRows) //проверяем содержит ли одну или несколько строк
-            {
-                DataTable data = new DataTable();
-                data.Load(dataReader);
-                dataGridView1 = data;//таблица
-            }
-            command.Dispose();
-            sqlConnession.Close();//закрываем соединени с БД
-        }
     }
 }
